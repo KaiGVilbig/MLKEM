7de945e470c8444f4b6243e95d5f08ef70b3e7a0
Kai Vilbig <kaikundes18@gmail.com>
2025-03-10 21:06:48 -0400

    initial commit

---
+++ include/mlkem.h
@@ -0,0 +1,16 @@
+﻿#ifndef MLKEM_H
+#define MLKEM_H
+
+#ifdef _WIN32
+#define EXPORT_API __declspec(dllexport)
+#else
+#define EXPORT_API __attribute__((visibility("default")))
+#endif
+
+extern "C" {
+    EXPORT_API void kemKeyGen();
+    EXPORT_API void kemEncaps();
+    EXPORT_API void kemDecaps();
+}
+
+#endif // MLKEM_H
---
+++ src/mlkem.cpp
@@ -0,0 +1,18 @@
+﻿#include "mlkem.h"
+#include <iostream>
+
+extern "C" {
+
+    EXPORT_API void kemKeyGen() {
+        std::cout << "Generating ML-KEM key pair..." << std::endl;
+    }
+
+    EXPORT_API void kemEncaps() {
+        std::cout << "Encapsulating key..." << std::endl;
+    }
+
+    EXPORT_API void kemDecaps() {
+        std::cout << "Decapsulating key..." << std::endl;
+    }
+
+}
