6a5f799243378bcf0e7e6114bd2a5bee982423d7
Kai <kaikundes18@gmail.com>
2025-03-27 10:58:09 -0400

    bits byte conversions

--- include/byteEncode.h
+++ include/byteEncode.h
@@ -6,5 +6,7 @@
 
 void byteEncode();
 void byteDecode();
+uint64_t bytesToBits(const std::vector<uint8_t>, size_t);
+std::vector<uint8_t> bitsToBytes(uint64_t, size_t);
 
 #endif // BYTE_ENCODE_H
--- src/byteEncode.cpp
+++ src/byteEncode.cpp
@@ -8,3 +8,27 @@ void byteEncode() {
 void byteDecode() {
     std::cout << "[INFO] ByteDecode() called\n";
 }
+
+uint64_t bytesToBits(const std::vector<uint8_t> bytes, size_t bitLength) {
+    uint64_t bits = 0;
+    size_t byteLength = (bitLength + 7) / 8;
+
+    for (size_t i = 0; i < bitLength; i++) {
+        if (bytes[i / 8] & (1 << (i % 8))) {
+            bits |= (1ULL << i);
+        }
+    }
+    return bits;
+}
+
+std::vector<uint8_t> bitsToBytes(uint64_t bits, size_t bitLength) {
+    size_t byteLength = (bitLength + 7) / 8; // Round up to nearest byte
+    std::vector<uint8_t> bytes(byteLength, 0);
+
+    for (size_t i = 0; i < bitLength; i++) {
+        if (bits & (1ULL << i)) {
+            bytes[i / 8] |= (1 << (i % 8));
+        }
+    }
+    return bytes;
+}
