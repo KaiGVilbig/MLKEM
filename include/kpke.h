341593e6a0133bb4fee7903fc0ec2936dfe90291
Kai Vilbig <kaikundes18@gmail.com>
2025-04-25 17:08:09 -0400

    start kpke

--- include/kpke.h
+++ include/kpke.h
@@ -1,6 +1,11 @@
 #include "ntt.h"
 #include "byteEncode.h"
+#include "sampling.h"
+#include "hash.h"
+#include <vector>
 
-void kpkeKeyGen();
+enum class Variants {MLKEM512, MLKEM768, MLKEM1024};
+
+std::pair<std::vector<uint8_t>, std::vector<uint8_t>> kpkeKeyGen(uint8_t, Variants);
 void kpkeEncrypt();
 void kpkeDecrypt();
--- src/kpke.cpp
+++ src/kpke.cpp
@@ -1,8 +1,40 @@
 #include "kpke.h"
 #include <iostream>
 
-void kpkeKeyGen() {
-	std::cout << "[INFO] kpkeKeyGen() called\n";
+std::tuple<int, int, int, int> getVariant(Variants variant) {
+	int k, n1, du, dv;
+	switch (variant) {
+		case Variants::MLKEM512:
+			k = 2, n1 = 3, du = 10, dv = 4;
+			break;
+		case Variants::MLKEM768:
+			k = 3, n1 = 2, du = 10, dv = 4;
+			break;
+		case Variants::MLKEM1024:
+			k = 4, n1 = 2, du = 11, dv = 5;
+		default:
+			k = 2, n1 = 3, du = 10, dv = 4;
+			break;
+	}
+	return {k, n1, du, dv};
+}
+
+std::pair<std::vector<uint8_t>, std::vector<uint8_t>> kpkeKeyGen(std::vector<uint8_t> d, Variants variant) {
+	std::pair<std::vector<uint8_t>, std::vector<uint8_t>> pa = G(d);
+	int N = 0, k, n1, du, dv;
+
+	std::tie(k, n1, du, dv) = getVariant(variant);
+	std::vector<std::vector<uint16_t>> Ahat;
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < k; j++) {
+			Ahat.push_back(SampleNTT(pa.first));
+		}
+	}
+
+	std::vector<std::vector<uint16_t>> s;
+	for (int i = 0; i < k; i++) {
+		s.push_back(samplePolyCBD(J(pa.second, N)));
+	}
 }
 
 void kpkeEncrypt() {
